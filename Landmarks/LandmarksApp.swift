bf5860b742405178441d7de5a61522d915ff031a
Jonathan Taveras <jonathanrtv@outlook.com>
2022-11-09 15:18:47 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jonathan Taveras Vargas on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
